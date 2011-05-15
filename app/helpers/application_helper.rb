6061af666411f6760ca029607b8e4acba0432338
stepan V <3dstepych@gmail.com>
2011-05-16 01:51:55 +0400

    Removed default Rails page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
-module ApplicationHelper
-end
+ module ApplicationHelper
+
+    # Return a title on a per-page basis.               # Комментарий к документу
+    def title                                           # определение метода
+      base_title = "Ruby on Rails Tutorial Sample App"  # назначение переменной
+      if @title.nil?                                    # булевый тест на nil
+	base_title                                      # неявное возвращение
+      else
+	"#{base_title} | #{@title}"                     # интерполяция строки
+      end
+    end
+  end
